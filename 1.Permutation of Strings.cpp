cc8fc51e5e798d4d8f7625f39f814d1941a1d8ea
Naveed Waddo <109472792+NaveedWaddo@users.noreply.github.com>
2023-12-02 15:14:55 +0530

    Update 1.Permutation of Strings.cpp

--- 1.Permutation of Strings.cpp
+++ 1.Permutation of Strings.cpp
@@ -1,1 +1,28 @@
+// Optimale Approach : Backtracking
+class Solution{
+private:
+    void permute(string ip , string op , vector<string>&ans){
+        if(ip.size() == 0){
+            ans.push_back(op);
+            return;
+        }
+        
+        unordered_set<char>st;
+        for(int i=0; i<ip.size(); i++){
+            if(st.find(ip[i]) == st.end()){
+                string newIp = ip.substr(0,i) + ip.substr(i+1,ip.size());
+                string newOp = op+ip[i];
+                st.insert(ip[i]);
+                
+                permute(newIp , newOp , ans);
+            }
+        }
+    }
 
+public:
+	vector<string>find_permutation(string S){
+	    vector<string>ans;
+	    permute(S , "" , ans);
+	    return ans;
+	}
+};
